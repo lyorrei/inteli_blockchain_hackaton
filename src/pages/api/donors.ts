db970be7ffac1339d4fc931dc4bd85fabd5ffe20
lyorrei-inteli <reishono@outlook.com>
2022-09-11 09:40:24 -0300

    Correção de erro de tipagem

--- src/pages/api/donors.ts
+++ src/pages/api/donors.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 export default async function handler(req, res) {
     // Criptografar a informação
-    const decoded = await jwt.verify(req.body.hashedName, process.env.JWT_INFO_SECRET)
+    const decoded: any = await jwt.verify(req.body.hashedName, process.env.JWT_INFO_SECRET)
 
     res.status(200).json({ name: decoded.name })
 }
